5faca5c7053595fe8700bc59716dac48fb3befde
fever-max <131164588+fever-max@users.noreply.github.com>
2024-06-04 16:37:30 +0900

    Feat: 게시글 작성, 반환, 삭제 구현

---
+++ src/main/java/com/cine/back/board/dto/BoardDto.java
@@ -0,0 +1,16 @@
+package com.cine.back.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDto {
+
+    private String boardTitle;
+
+    private String boardContent;
+
+    private String userEmail;
+
+    private String boardListTag;
+
+}
