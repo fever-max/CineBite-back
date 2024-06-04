58f4d0749ff69cf7800e76d919cce0ed1b0f0f7f
Seokki97 <skaksdl1238@gmail.com>
2024-06-04 22:03:34 +0900

    feat - ExceptionHandler 설정

---
+++ src/main/java/handler/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+    /**
+     * ExceptionHandler
+     * required : HttpStatus, Exception e
+     * @ExceptionHandler()
+     */
+}
