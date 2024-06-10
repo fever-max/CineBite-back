3b96b29983c4303d9a94cf7285d9d1a0c4317361
Uangi <jyp423@naver.com>
2024-06-10 12:03:45 +0900

    Comment : @Embeddable

--- src/main/java/com/cine/back/movieList/dto/Credits.java
+++ src/main/java/com/cine/back/movieList/dto/Credits.java
@@ -9,10 +9,13 @@ import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
-@Embeddable
+@Embeddable // 다른 클래스에 포함
 public class Credits {
     
     @JsonProperty("cast")
     @Transient
     private List<Cast> cast;
+
+    // 필드가 하나이기 때문에 클래스 자체에 @Embeddable을 사용하는 것.
+    // 필드가 복수 개라면 적용할 필드 값에 @Embedded를 사용하면 된다.
 }
