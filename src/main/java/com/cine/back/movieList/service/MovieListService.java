244026cb02dc08a6734fdd54fda0a61ecce79a62
Uangi <jyp423@naver.com>
2024-06-17 23:08:27 +0900

    comment : 메서드 설명 추가

--- src/main/java/com/cine/back/movieList/service/MovieListService.java
+++ src/main/java/com/cine/back/movieList/service/MovieListService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import com.cine.back.movieList.entity.MovieDetailEntity;
 import com.cine.back.movieList.repository.MovieDetailRepository;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class MovieListService {
@@ -15,17 +17,17 @@ public class MovieListService {
         this.movieDetailRepository = movieDetailRepository;
     }
 
-    public List<MovieDetailEntity> getMovieGernes(String genre){
+    public List<MovieDetailEntity> getMovieGernes(String genre){    // 장르 별 데이터 목록 불러오기
         List<MovieDetailEntity> movieGenres = movieDetailRepository.findByGenres(genre);
         return movieGenres;
     }
 
-    public List<MovieDetailEntity> getMovieActors(String actor){
+    public List<MovieDetailEntity> getMovieActors(String actor){    // 배우 별 데이터 목록 불러오기
         List<MovieDetailEntity> movieActors = movieDetailRepository.findByActors(actor);
         return movieActors;
     }
 
-    public Optional<MovieDetailEntity> getMovieDetail(int movieId){
+    public Optional<MovieDetailEntity> getMovieDetail(int movieId){     // 영화 상세정보 번호 불러오기
         Optional<MovieDetailEntity> movieDetail = movieDetailRepository.findByMovieId(movieId);
         return movieDetail;
     }
