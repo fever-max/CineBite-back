2cefc395ea41f532e91d5c263e98fb432fb89ded
Uangi <jyp423@naver.com>
2024-06-21 00:04:26 +0900

    feat : 로튼 토마토 평가 컨트롤러 구현

--- src/main/java/com/cine/back/movieList/controller/TomatoController.java
+++ src/main/java/com/cine/back/movieList/controller/TomatoController.java
@@ -1,32 +1,32 @@
-// package com.cine.back.movieList.controller;
+package com.cine.back.movieList.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.cine.back.movieList.service.EvaluateService;
+import com.cine.back.movieList.service.EvaluateService;
 
-// @RestController
-// @RequestMapping("/movies")
-// public class TomatoController {
+@RestController
+@RequestMapping("/movies")
+public class TomatoController {
 
-//     @Autowired
-//     private EvaluateService evaluateService;
+    @Autowired
+    private EvaluateService evaluateService;
 
-//     @PostMapping("/{movieId}/rate")
-//     public ResponseEntity<String> rateMovie(
-//             @RequestParam String userId,
-//             @PathVariable int movieId,
-//             @RequestParam String rating) {
-//         try {
-//             evaluateService.rateMovie(userId, movieId, rating);
-//             return ResponseEntity.ok("평가가 성공적으로 저장되었습니다.");
-//         } catch (Exception e) {
-//             return ResponseEntity.badRequest().body(e.getMessage());
-//         }
-//     }
-// }
+    @PostMapping("/{movieId}/rate")
+    public ResponseEntity<String> rateMovie(
+            @RequestParam String userId,
+            @PathVariable int movieId,
+            @RequestParam String rating) {
+        try {
+            evaluateService.rateMovie(movieId, userId, rating);
+            return ResponseEntity.ok("평가가 성공적으로 저장되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
