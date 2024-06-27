731606eea0ad75a7545c16282a1a109466555d63
50104 <solaya7@naver.com>
2024-06-28 01:11:02 +0900

    Feat : 로그인 API, jwt 토큰 구현

--- src/main/java/com/cine/back/config/WebSecurityConfig.java
+++ src/main/java/com/cine/back/config/WebSecurityConfig.java
@@ -1,30 +1,42 @@
 package com.cine.back.config;
 
-
 import java.io.IOException;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
-import com.cine.back.user.filter.JwtAuthenticationFilter;
+import com.cine.back.user.filter.CustomLogoutFilter;
+import com.cine.back.user.filter.JwtFilter;
+import com.cine.back.user.filter.LoginFilter;
 import com.cine.back.user.handler.OAuth2SuccessHandler;
+import com.cine.back.user.provider.JwtProvider;
+import com.cine.back.user.repository.RefreshRepository;
+import com.cine.back.user.service.implement.CustomUserDetailsService;
+import com.cine.back.user.service.implement.OAuth2UserServiceImplement;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -36,10 +48,25 @@ import jakarta.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtProvider jwtProvider;
     private final DefaultOAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final com.cine.back.user.service.implement.OAuth2UserServiceImplement OAuth2UserServiceImplement;
+    private final OAuth2UserServiceImplement oAuth2UserServiceImplement;
+    private final RefreshRepository refreshRepository;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
@@ -53,31 +80,31 @@ public class WebSecurityConfig {
             .sessionManagement(sessionManagement -> sessionManagement
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
-            // .addFilterAfter(new JWTFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class) // 재로그인, 무한로프 방지 // xx 추가
 
             .authorizeHttpRequests(request -> request
-                .requestMatchers("/","/api/v1/auth/**","/oauth2/**").permitAll()
+                .requestMatchers("/", "/login", "/reissue", "/api/v1/auth/**","/oauth2/**", "/error").permitAll()
+                .requestMatchers(HttpMethod.GET,"/api/user/**").permitAll()
                 .requestMatchers("/api/v1/user/**").hasRole("USER") 
                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
             )
             .oauth2Login(oauth2 -> oauth2
                 .authorizationEndpoint(endpoint -> endpoint.baseUri("/api/v1/auth/oauth2")) 
                 .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
                 .userInfoEndpoint(endpoint -> endpoint.userService(oAuth2UserService)
-                .userService(OAuth2UserServiceImplement)) // OAuth2UserService 설정) // xx 추가
+                .userService(oAuth2UserServiceImplement)) // OAuth2UserService 설정) // xx 추가
                 .successHandler(oAuth2SuccessHandler) 
             )
             .exceptionHandling(exceptionHandling -> exceptionHandling
                 .authenticationEntryPoint(new FailedAuthenticationEntryPoint())
             )
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터 추가
+            .addFilterAt(new LoginFilter(authenticationManager(), jwtProvider, refreshRepository), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new JwtFilter(jwtProvider), LoginFilter.class) // JWT 인증 필터 추가
 
             // xx 추가
             .csrf((auth) -> auth.disable())
             .formLogin((auth) -> auth.disable())
             .httpBasic((auth) -> auth.disable());
-
         return httpSecurity.build();
     }
 
@@ -87,13 +114,13 @@ public class WebSecurityConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("http://localhost:3000");
         corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedHeader("*"); 
+        corsConfiguration.setAllowCredentials(true); // xx 추가 인증정보 포함 여부 
 
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setMaxAge(3600L); // xx 추가
         corsConfiguration.setExposedHeaders(Arrays.asList(
-            "Set-Cookie", // Set-Cookie 헤더를 노출
-            "Authorization"  // Authorization에 jwt 담아서 보냄 // Authorization 헤더를 노출
+            "Set-Cookie", "Authorization", "access", "refresh"
             /* ,"Access-Control-Allow-Headers",
                 "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
                 "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" */
@@ -112,7 +139,6 @@ class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN); 
         response.getWriter().write("{\"code\": \"NP\", \"message\": \"No Permission.\"}");
